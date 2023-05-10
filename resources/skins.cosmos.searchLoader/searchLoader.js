2582958d9bce0b0c6019795c807797ede3e91957
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-05-10 16:11:39 -0600

    Update searchLoader.js

--- resources/skins.cosmos.searchLoader/searchLoader.js
+++ resources/skins.cosmos.searchLoader/searchLoader.js
@@ -47,11 +47,11 @@ function loadSearchModule( element, moduleName, afterLoadFn ) {
 
 		if ( $( window ).width() < 851 ) {
 			var inputCheck = setInterval( function () {
-				if ( document.getElementsByClassName( 'wvui-input__input' )[ 0 ] !== undefined ) {
+				if ( document.getElementsByClassName( 'cdx-text-input__input' )[ 0 ] !== undefined ) {
 					clearInterval( inputCheck );
 
-					document.getElementsByClassName( 'wvui-input__input' )[ 0 ].addEventListener( 'focus', onFocus );
-					document.getElementsByClassName( 'wvui-input__input' )[ 0 ].addEventListener( 'focusout', onFocusOut );
+					document.getElementsByClassName( 'cdx-text-input__input' )[ 0 ].addEventListener( 'focus', onFocus );
+					document.getElementsByClassName( 'cdx-text-input__input' )[ 0 ].addEventListener( 'focusout', onFocusOut );
 				}
 			}, 100 );
 		}
